8fd7480facbd3d734f31ebdf4bcabe069d94a1df
Murarth <murarth@gmail.com>
2020-06-26 11:00:40 -0700

    Add `std` feature, disabled by default
    * Fixes `std` being included with `serde`

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 //!
 //! ## `no_std` support
 //!
-//! By default, `smallstr` does not depend on `std`. However, the `ffi` feature will add
-//! `std` as a dependency.
+//! By default, `smallstr` does not depend on `std`. The `std` feature may be enabled
+//! to add the `std` dependency. The `ffi` feature also implies `std`.
 //!
 //! ## `ffi` feature
 //!
@@ -23,18 +23,18 @@
 //!
 //! This feature is disabled by default.
 //!
+//! By default, the `serde` dependency is compiled with `no_std`.
+//! If the `std` feature is enabled, `std` is added as a dependency in `serde`, as well.
+//!
 //! ## `union` feature
 //!
 //! This feature will enable the `union` feature in `smallvec`, which reduces the size of
 //! a `SmallString` instance. This feature requires a nightly compiler.
 
-#![no_std]
+#![cfg_attr(not(any(feature = "ffi", feature = "std")), no_std)]
 #![deny(missing_docs)]
 
 extern crate alloc;
-
-#[cfg(any(feature = "error", feature = "ffi",))]
-extern crate std;
 
 pub use string::*;
 
